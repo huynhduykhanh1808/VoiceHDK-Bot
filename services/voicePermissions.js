37b360c13cdf770d518721dfbf4c4885302057d5
huynhduykhanh1808 <huynhduykhanhgame@gmail.com>
2026-09-25 01:49:29 +0700

    Update voicePermissions.js

--- services/voicePermissions.js
+++ services/voicePermissions.js
@@ -1,1 +1,21 @@
+'use strict';
 
+// Single source of truth for participant voice capabilities. Lock/hide only
+// change Connect/ViewChannel and must not strip these capabilities.
+const NORMAL_VOICE_ACCESS = Object.freeze({
+  ViewChannel: true,
+  Connect: true,
+  Speak: true,
+  UseVAD: true,
+  Stream: true
+});
+
+function normalVoiceAccess(extra = {}) {
+  return { ...NORMAL_VOICE_ACCESS, ...extra };
+}
+
+function normalVoiceFlags(flags) {
+  return [flags.ViewChannel, flags.Connect, flags.Speak, flags.UseVAD, flags.Stream];
+}
+
+module.exports = { NORMAL_VOICE_ACCESS, normalVoiceAccess, normalVoiceFlags };
